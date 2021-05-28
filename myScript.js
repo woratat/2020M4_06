ca6d53717aa2d58db3d53b0a28bc18c19acc4ca6
woratat <84006033+woratat@users.noreply.github.com>
2021-05-28 17:28:15 +0700

    06 add click event

--- myScript.js
+++ myScript.js
@@ -1,15 +1,14 @@
-// document.getElementById('ok').addEventListener('click',function(e){
-//     alert(document.getElementById('ok').id);
-// }
-// )
-// document.getElementById('cancel').addEventListener('click',function (){
-//     alert(document.getElementById('cancel').id);
-// });
-// document.getElementById('no').addEventListener('click',function (){
-//     alert(document.getElementById('no').id);
-// });
-
-
 document.getElementById('cancel').addEventListener('mousemove',function(e){
     console.log(`x = ${e.clientX} y = ${e.clientY}`);
 })
+
+document.getElementById('ok').addEventListener('click',function(e){
+    alert(document.getElementById('ok').id);
+}
+)
+document.getElementById('cancel').addEventListener('click',function (){
+    alert(document.getElementById('cancel').id);
+});
+document.getElementById('no').addEventListener('click',function (){
+    alert(document.getElementById('no').id);
+});
